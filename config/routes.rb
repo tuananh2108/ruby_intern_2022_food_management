5e3a196bde3b10b3a87c7c9b81577d6a763ebfb9
tuananh2108 <htg2182000@gmail.com>
2022-07-21 17:00:01 +0700

    Add New Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
-    root "admin_static_pages#home"
-    get "/home", to: "admin_static_pages#home"
+    namespace :admin do
+      root "admin_static_pages#home"
+      get "/home", to: "admin_static_pages#home"
+      resources :products
+    end
+
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
-
-    resources :products
   end
 end
